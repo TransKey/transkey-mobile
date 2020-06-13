e4e6ad0215cfc95d27cebcf7ce6a79124929b8df
lamecksilva <lameck2@hotmail.com>
2020-06-13 18:00:01 -0300

    FEAT: MapView

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-import {View, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+import { View } from 'native-base';
+
+import styles from './styles';
 
 export function HomeScreen() {
   return (
     <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}></MapView>
+      <MapView style={styles.map} />
     </View>
   );
 }
